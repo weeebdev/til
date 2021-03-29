0636f58d4e8c765337216ea81ca32568ba8bc46d
Adil <mr.adil777@gmail.com>
2021-03-29 17:35:43 +0600

    feat(jwt): add jwt

---
+++ js/jwt/README.md
@@ -0,0 +1,1 @@
+[Node.js API Authentication With JWT](https://www.youtube.com/watch?v=7nafaH9SddU)
---
+++ js/jwt/test.sh
@@ -0,0 +1,2 @@
+curl --location --request POST 'http://localhost:5000/api/posts' \
+--header 'Authorization: Bearer TOKEN'
